a085cf98cd0310203cfd8f8218aabda1b67503c9
Suraj Kumar aka Steuxnet <imsurajkc@gmail.com>
2022-09-20 08:10:29 +0530

    Time: 6414 ms (42.82%), Space: 39.2 MB (74.09%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,17 +1,8 @@
-class Solution:                 # 1) We build memo, a 2Darray, 2) iterate thru nums1 & nums2
-                                # in reverse to populate memo, and then 3) find the max element
-                                # in memo; its row and col in memo shows the starting indices
-                                # for the common seq in nums1 and nums2.  
-
-    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        
-        n1, n2 = len(nums1), len(nums2)          
-        memo = [[0]*(n2+1) for _ in range(n1+1)]                    # <-- 1)
-        
-        for idx1 in range(n1)[::-1]:
-            for idx2 in range(n2)[::-1]:
-                
-                if nums1[idx1] == nums2[idx2]:
-                    memo[idx1][idx2] = 1 + memo[idx1+1][idx2+1]     # <-- 2)
-       
-        return max(chain(*memo))                           
+class Solution(object):
+    def findLength(self, A, B):
+        memo = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+        for i in range(len(A) - 1, -1, -1):
+            for j in range(len(B) - 1, -1, -1):
+                if A[i] == B[j]:
+                    memo[i][j] = memo[i + 1][j + 1] + 1
+        return max(max(row) for row in memo)
